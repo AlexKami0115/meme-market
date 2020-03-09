71e7a9252c0fc010d2aa5b5d5b8e074324e34482
alexkami <alexkamien@outlook.com>
2020-03-09 10:41:46 -0400

    User and Card hibernate done

---
+++ MemeMarket/src/main/java/com/revature/data/CardHibernate.java
@@ -0,0 +1,73 @@
+package com.revature.data;
+
+import java.awt.print.Book;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.revature.beans.Card;
+import com.revature.utils.HibernateUtil;
+import com.revature.utils.LogUtil;
+
+public class CardHibernate implements CardDao{
+	private HibernateUtil hu = HibernateUtil.getInstance();
+	
+	@Override
+	public int addCard(Card c) {
+		Session s = hu.getSession();
+		Transaction t = null;
+		Integer i = 0;
+		try {
+			t = s.beginTransaction();
+			i = (Integer) s.save(c);
+			t.commit();
+		} catch(HibernateException e) {
+			t.rollback();
+			LogUtil.logException(e, UserHibernate.class);
+		} finally {
+			s.close();
+		}
+		return i;
+	};
+	
+	@Override
+	public Card getCard(int id) {
+		Session s = hu.getSession();
+		Card c = s.get(Card.class, id);
+		s.close();
+		return c;
+	};
+	
+	public void updateCard(Card c) {
+		Session s = hu.getSession();
+		Transaction t = null;
+		try{
+			t = s.beginTransaction();
+			s.update(c.getId());
+			t.commit();
+		} catch(Exception e) {
+			if(t != null)
+				t.rollback();
+			LogUtil.logException(e, UserHibernate.class);
+		} finally {
+			s.close();
+		}
+	};
+	public void deleteCard(Card c) {
+		Session s = hu.getSession();
+		Transaction t = null;
+		try{
+			t = s.beginTransaction();
+			s.delete(c.getId());
+			t.commit();
+		} catch(Exception e) {
+			if(t != null)
+				t.rollback();
+			LogUtil.logException(e, UserHibernate.class);
+		} finally {
+			s.close();
+		}
+	};
+
+}
