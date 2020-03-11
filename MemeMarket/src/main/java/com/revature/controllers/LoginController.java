f2c24da30a980a60e9d5b28d331163d23c9a8025
alexkami <alexkamien@outlook.com>
2020-03-11 14:08:04 -0400

    error fix

--- MemeMarket/src/main/java/com/revature/controllers/LoginController.java
+++ MemeMarket/src/main/java/com/revature/controllers/LoginController.java
@@ -1,6 +1,7 @@
 package com.revature.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.services.UserService;
 
