5f341a1f65d4fb2b92a153fd007e6f1a5ec79ed5
Wideman <wideman.christopher.roy@gmail.com>
2020-03-20 14:15:34 -0400

    getters and setters

--- MemeMarket/src/main/java/com/revature/beans/Comment.java
+++ MemeMarket/src/main/java/com/revature/beans/Comment.java
@@ -30,6 +30,12 @@ public class Comment {
 	@Column
 	private Timestamp timeOfComment;
 	
+	public Integer getCardId() {
+		return cardId;
+	}
+	public void setCardId(Integer cardId) {
+		this.cardId = cardId;
+	}
 	private Integer cardId;
 	
 	public Comment() {
