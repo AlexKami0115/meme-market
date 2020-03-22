68fec9e1ee8c505428b865335b0d70b92e38098f
jdelgado <delgado.jeffery@gmail.com>
2020-03-22 15:28:05 -0400

    Jeff's last commit

--- MemeMarket/src/main/java/com/revature/controllers/TradeController.java
+++ MemeMarket/src/main/java/com/revature/controllers/TradeController.java
@@ -1,13 +1,19 @@
 package com.revature.controllers;
 
+import java.util.Set;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.revature.beans.Trade;
+import com.revature.beans.User;
 import com.revature.services.TradeService;
 
 @RestController
@@ -20,5 +26,12 @@ public class TradeController {
 	private ResponseEntity<Trade> addCard(@RequestBody Trade t) {
 		ts.addTrade(t);
 		return ResponseEntity.ok(t);
+	}
+	
+	@GetMapping(path="/trade")
+	private ResponseEntity<Set<Trade>> getTrades(HttpSession session) {
+		User u = (User) session.getAttribute("loggedUser");
+		if (u == null) return ResponseEntity.badRequest().build();
+		return ResponseEntity.ok(ts.getTrades(u.getId()));
 	}
 }
